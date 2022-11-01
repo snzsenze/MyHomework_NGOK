63919d08a2a4a5a8427bf4705154e2e0172e7814
Uncori <102057142+Uncori@users.noreply.github.com>
2022-11-01 22:43:20 +0700

    Update and rename func.h to array.h
    -_-

--- Array/array.h
+++ Array/array.h
@@ -1,16 +1,14 @@
-#ifndef _FUNC_H_
-#define _FUNC_H_
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
 
-//////////////////////////////////////////////////
-// допиши аргументы
 void set_array(int *array, const unsigned int size);
 void print_array(const int array[], const unsigned int size);
 int search_max(const int array[], const unsigned int size);
 int search_min(const int array[], const unsigned int size);
 void reserve_array(int *array, const unsigned int size);
 int search_index(const int array[], const unsigned int size, const int number);
-int comparsion_array(const int array[], const unsigned int PUP,
-               const int double_array[], const unsigned int PUPU);
+int comparsion_array(const int arrayFirst[], const unsigned int sizeFirst,
+               const int arraySecond[], const unsigned int sizeSecond);
 void bubble_sort(int *array, const unsigned int size);
 int binary_search(int array[], int size, int number);
 
