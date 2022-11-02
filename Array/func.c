0b7abe4d5db322e0904034ba71bea7abf01c302c
Nikita <snzsenze@mail.ru>
2022-11-02 15:13:21 +0700

    123

--- Array/func.c
+++ Array/func.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void set_array(int *array, const unsigned int size){
+void set_array(int *array, const unsigned int size)
+{
 
     for (unsigned int i = 0; i < size; ++i) {
         printf(" Вводите числа через Enter ");
@@ -11,14 +12,16 @@ void set_array(int *array, const unsigned int size){
     }
 }
 
-void print_array(const int array[], const unsigned int size){
+void print_array(const int array[], const unsigned int size)
+{
     for (unsigned int i = 0; i < size; ++i) {
         printf(" |%d| ", array[i]);
     }
     printf("\n");
 }
 
-int search_max(const int array[], const unsigned int size){
+int search_max(const int array[], const unsigned int size)
+{
     int max = array[0];
     for (unsigned int i = 1; i < size; ++i) {
         if (max < array[i]) {
@@ -28,7 +31,8 @@ int search_max(const int array[], const unsigned int size){
     return max;
 }
 
-int search_index(const int array[], const unsigned int size, const int number){
+int search_index(const int array[], const unsigned int size, const int number)
+{
     int index = 0;
     for (unsigned int i = 0; i < size; ++i) {
         if (array[i] == number) {
@@ -50,7 +54,7 @@ int search_min(const int array[], const unsigned int size)
     return min;
 }
 
-void resereserve_array(int *array, const unsigned int size)
+void reserve(int *array, const unsigned int size)
 {
     int go = array[0];
     for (unsigned int i = 0, j = size - 1; i < size / 2; ++i, --j) {
@@ -60,7 +64,9 @@ void resereserve_array(int *array, const unsigned int size)
     }
 }
 
-int comparsion_array(const int array[], const unsigned int PUP, const int double_array[], const unsigned int PUPU){
+int comparsion_array(const int array[], const unsigned int PUP,
+                     const int double_array[], const unsigned int PUPU)
+{
     int res = 1;
 
     if (PUP == PUPU) {
@@ -76,48 +82,59 @@ int comparsion_array(const int array[], const unsigned int PUP, const int double
     return res;
 }
 
-void bubble_sort(int *array, const unsigned int size){
+void bubble_sort(int *array, const unsigned int size)
+{
 
     // Создали переменную для хранения
     int tmp = 0;
 
     // Цикл Без знаковый И = 0 если И меньше размера-1 тб(10-1)(9) то И++
-    for(unsigned int i = 0; i < size - 1; i++){
-        for(unsigned int j = 0; j < size - i - 1; j++){
-   // Если элемент Джи больше следующего элемента то
-            if(array[j] > array[j+1]){
-                tmp = array[j];     // записываем в переменную тмп наш элемент
-                array[j] = array[j+1]; // элемент будет равен другому тб(было 10 2\ стало 2 10)
-                array[j+1] = tmp;   // а след элемент будет равен тому что мы записали на 2ст выше!(tmp = array[j];)
-                tmp = array[j+1];   // БЕЗ ЭТОГО ТОЖЕ РОБИТ :)
+    for (unsigned int i = 0; i < size - 1; i++) {
+        for (unsigned int j = 0; j < size - i - 1; j++) {
+            // Если элемент Джи больше следующего элемента то
+            if (array[j] > array[j + 1]) {
+                tmp = array[j]; // записываем в переменную тмп наш элемент
+                array[j] = array[j + 1]; // элемент будет равен другому тб(было
+                                         // 10 2\ стало 2 10)
+                array[j + 1] = tmp; // а след элемент будет равен тому что мы
+                                    // записали на 2ст выше!(tmp = array[j];)
+                tmp = array[j + 1]; // БЕЗ ЭТОГО ТОЖЕ РОБИТ :)
             }
-
         }
     }
 }
 
-int binary_search(int array[], int size, int number){
+int binary_search(int array[], int size, int number)
+{
 
-    int min_i = 0; // нашли минимальное
-    int max_i = size;  // максимальное
+    int min_i = 0;         // нашли минимальное
+    int max_i = size;      // максимальное
     int mid_i = max_i / 2; // находим центр
- 
-    int res = -1;  
 
-    while (1)
-    {
-        if (number > array[mid_i]){  // если число которрое мы ввели больше центра то
-            min_i = mid_i;                          // минимальное будет равно центральному
-            mid_i = (min_i + max_i) / 2;                // и новый мид будет равен (если сложить минимальное и максимальное) и / 2 
-        }else if( number == array[mid_i]){      // Иначе если (число которрое мы ввели будет равно нашему центру) то
-            res = mid_i;                        // результат будет равен цент знач!
-            break;                              // и сразу брейкаем
-        }else{                                  // Иначе 
-            max_i = mid_i;                      // Наше максимальное будет равно центру а
-            mid_i = (max_i + mid_i - 1) / 2;    // центр будет равен (если сложить максимальное и центр-1) и / 2
+    int res = -1;
+
+    while (1) {
+        if (number >
+            array[mid_i]) { // если число которрое мы ввели больше центра то
+            min_i = mid_i; // минимальное будет равно центральному
+            mid_i =
+                (min_i + max_i) / 2; // и новый мид будет равен (если сложить
+                                     // минимальное и максимальное) и / 2
+        } else if (number ==
+                   array[mid_i]) { // Иначе если (число которрое мы ввели будет
+                                   // равно нашему центру) то
+            res = mid_i; // результат будет равен цент знач!
+            break; // и сразу брейкаем
+        } else {   // Иначе
+            max_i = mid_i; // Наше максимальное будет равно центру а
+            mid_i = (max_i + mid_i - 1) / 2; // центр будет равен (если сложить
+                                             // максимальное и центр-1) и / 2
         }
-        if ((mid_i == 0 || mid_i == size - 1) && number != array[mid_i]){   // если наш мид == 0 или мид == размеру масива) и число которое мы ввели не равно центру нашего массива то
-            break;                                                                                         // брейкаем
+        if ((mid_i == 0 || mid_i == size - 1) &&
+            number != array[mid_i]) { // если наш мид == 0 или мид == размеру
+                                      // масива) и число которое мы ввели не
+                                      // равно центру нашего массива то
+            break; // брейкаем
         }
     }
     return res; // возвращаем наш рес
--- Array/func.h
+++ Array/func.h
@@ -7,12 +7,11 @@ void set_array(int *array, const unsigned int size);
 void print_array(const int array[], const unsigned int size);
 int search_max(const int array[], const unsigned int size);
 int search_min(const int array[], const unsigned int size);
-void reserve_array(int *array, const unsigned int size);
+void reserve(int *array, const unsigned int size);
 int search_index(const int array[], const unsigned int size, const int number);
 int comparsion_array(const int array[], const unsigned int PUP,
-               const int double_array[], const unsigned int PUPU);
+                     const int double_array[], const unsigned int PUPU);
 void bubble_sort(int *array, const unsigned int size);
 int binary_search(int array[], int size, int number);
-
 
 #endif // _FUNC_H_
